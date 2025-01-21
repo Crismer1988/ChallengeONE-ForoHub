54d4b46915adda925a813b6083c26516b44d3fd2
crismer1988 <crismer1988@gmail.com>
2025-01-21 01:10:03 -0500

    Primer_commit

---
+++ Desafio-ForoHub/src/test/java/com/alura/desafios/Desafio_ForoHub/DesafioForoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.desafios.Desafio_ForoHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioForoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
